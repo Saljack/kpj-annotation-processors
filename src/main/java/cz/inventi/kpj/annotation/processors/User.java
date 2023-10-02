70586ef3a1a178a461e5a64d59167728bc6fa070
Jaroslav Chládek <chladekj@email.cz>
2023-10-02 20:01:29 +0200

    Solve the required tasks

--- src/main/java/cz/inventi/kpj/annotation/processors/User.java
+++ src/main/java/cz/inventi/kpj/annotation/processors/User.java
@@ -1,9 +1,17 @@
 package cz.inventi.kpj.annotation.processors;
 
+import lombok.*;
+
 /**
  * Add lombok for adding getters, setters, toString, hashCode, equals, no
  * argument constructor and all argument constructor
  */
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
   private String username;
