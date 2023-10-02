218d6eb820b9a16908a2bdafdea9bb6ed63b1ca5
Tomáš Dejmek <dejmektomas@gmail.com>
2023-10-02 20:08:37 +0200

    implement User and UserDTO bothways mappers

--- src/main/java/cz/inventi/kpj/annotation/processors/User.java
+++ src/main/java/cz/inventi/kpj/annotation/processors/User.java
@@ -1,16 +1,24 @@
 package cz.inventi.kpj.annotation.processors;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * Add lombok for adding getters, setters, toString, hashCode, equals, no
  * argument constructor and all argument constructor
  */
+//@Data Data annotation does not work for me on my setup.
+@AllArgsConstructor
+@Getter @Setter
+@ToString
+@EqualsAndHashCode
 public class User {
-
   private String username;
-
   private String firstname;
   private String lastname;
-
   private String phone;
 
 }
