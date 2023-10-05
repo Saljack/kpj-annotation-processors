9a1b7da31d69b3928a53e4dd8a96f375e30409ed
viktor7469 <viktor7469@gmail.com>
2023-10-05 11:56:12 +0200

    Implemented tasks to annotation Processors

---
+++ src/main/java/cz/inventi/kpj/annotation/processors/UserMapper.java
@@ -0,0 +1,20 @@
+package cz.inventi.kpj.annotation.processors;
+
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    @Mapping(source="firstname",target="name")
+    @Mapping(source="lastname",target="surname")
+    UserDTO userToUserDTO(User user);
+
+    @Mapping(source="name",target="firstname")
+    @Mapping(source="surname",target="lastname")
+    User userDTOToUser(UserDTO userDTO);
+}
