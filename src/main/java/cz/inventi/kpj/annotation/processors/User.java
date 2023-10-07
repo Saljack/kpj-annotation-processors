91fcb1024e5d15550cc254365fcd7efb20a351bc
Tuan Ha <hatuan@email.cz>
2023-10-07 20:20:38 +0200

    implement necessary methods using Lombdok

--- src/main/java/cz/inventi/kpj/annotation/processors/User.java
+++ src/main/java/cz/inventi/kpj/annotation/processors/User.java
@@ -1,14 +1,21 @@
 package cz.inventi.kpj.annotation.processors;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Add lombok for adding getters, setters, toString, hashCode, equals, no
  * argument constructor and all argument constructor
  */
+
+@Data
+@NoArgsConstructor
 public class User {
 
   private String username;
 
   private String firstname;
+
   private String lastname;
 
   private String phone;
